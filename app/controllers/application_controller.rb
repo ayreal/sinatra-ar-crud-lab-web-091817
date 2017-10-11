49d4f095d18b78efaf591b020c8b6d87ad2d95ef
Ariel Herman <aeh.herman@gmail.com>
2017-10-11 08:56:05 -0400

    working on show-id page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,32 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do 
-    
+  get '/' do
+
   end
+
+# CREATE
+  get '/posts/new' do
+    erb :new
+  end
+
+  post '/posts' do
+    #no instance variable needed because you are just talking to the db
+    post = Post.create(params)
+    redirect "/posts/#{post.id}"
+
+  end
+
+# SHOW
+  get '/posts' do
+    @posts = Post.all
+    # instance variables used for when you want to pass info to the views or another controller
+    erb :index
+  end
+
+  get 'posts/:id' do
+    @post = Post.find(params["id"])
+    erb :show
+  end
+
 end
